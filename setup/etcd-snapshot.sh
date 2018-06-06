2454dc7b61bbd8d944f3ea52251a39385e862d94
akihiro-ikezoe <akihiro-ikezoe@cybozu.co.jp>
2018-06-06 18:56:47 +0900

    fix

--- setup/etcd-snapshot.sh
+++ setup/etcd-snapshot.sh
@@ -5,7 +5,9 @@ set -ue
 SNAPSHOT=snapshot-$(date '+%Y%m%d_%H%M%S')
 ETCD_PASSWD=$(cat /usr/local/etc/etcd-snapshot.passwd)
 
-ETCDCTL_API=3 etcdctl --user backup:${ETCD_PASSWD} snapshot save /tmp/${SNAPSHOT}
-tar --remove-files -cvzf /var/lib/etcd-snapshot/${SNAPSHOT}.tar.gz /tmp/${SNAPSHOT}
+uuid=$(rkt list --no-legend | awk '{if ($4 == "running" && $2 == "etcd") print $1}')
+
+rkt enter --app=etcd $uuid etcdctl --user backup:${ETCD_PASSWD} snapshot save /var/lib/etcd/${SNAPSHOT}
+tar --remove-files -cvzf /var/lib/etcd-snapshot/${SNAPSHOT}.tar.gz /var/lib/etcd-container/${SNAPSHOT}
 
 find /var/lib/etcd-snapshot/ -mtime 14 -exec rm -f {} \;
