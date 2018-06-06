ba8213428fe25984f03452c8c95352049781dc0c
akihiro-ikezoe <akihiro-ikezoe@cybozu.co.jp>
2018-06-06 19:05:47 +0900

    error check

--- setup/etcd-snapshot.sh
+++ setup/etcd-snapshot.sh
@@ -6,6 +6,10 @@ SNAPSHOT=snapshot-$(date '+%Y%m%d_%H%M%S')
 ETCD_PASSWD=$(cat /usr/local/etc/etcd-snapshot.passwd)
 
 uuid=$(rkt list --no-legend | awk '{if ($4 == "running" && $2 == "etcd") print $1}')
+if [ -z "$uuid" ]; then
+  echo "could not find etcd container"
+  exit 1
+fi
 
 rkt enter --app=etcd $uuid etcdctl --user backup:${ETCD_PASSWD} snapshot save /var/lib/etcd/${SNAPSHOT}
 tar --remove-files -cvzf /var/lib/etcd-snapshot/${SNAPSHOT}.tar.gz /var/lib/etcd-container/${SNAPSHOT}
